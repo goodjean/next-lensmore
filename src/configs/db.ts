0aeb87f9f70735ea0c472813a93981db4b9b2444
goodjean <goodjean1@naver.com>
2023-03-19 14:44:46 +0900

    Feat: "Feat: login & signup finish"

---
+++ src/configs/db.ts
@@ -0,0 +1,6 @@
+export default {
+  host: "localhost",
+  user: "root",
+  password: "Op78520123!",
+  database: "crawler_test",
+};
---
+++ src/server/type/user.d.ts
@@ -0,0 +1,50 @@
+import { RowDataPacket } from "mysql2";
+
+export interface ILoginResponse {
+  id: string;
+  name: string;
+}
+
+export interface IRegisterUserEntity extends RowDataPacket {
+  user_id: string;
+  password: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface IRegisterUser {
+  user_id: string;
+  password: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface IFoundUserEntity extends RowDataPacket {
+  user_id: string;
+}
+
+export interface IFoundUser {
+  user_id: string;
+}
+
+export interface ILoginUserEntity extends RowDataPacket {
+  user_id: string;
+  password: string;
+  name: string;
+}
+
+export interface ILoginUser {
+  user_id: string;
+  password: string;
+  name: string;
+}
+
+export interface IUserIdEntity extends RowDataPacket {
+  user_id: string;
+}
+
+export interface IUserId {
+  user_id: string;
+}
