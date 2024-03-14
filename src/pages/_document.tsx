764b9f7d4bb4e5f1c1949979045681707c193001
goodjean <goodjean1@naver.com>
2024-03-14 20:18:20 +0900

    add og-tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,9 @@ export default function Document() {
         <meta charSet="UTF-8" />
         <meta property="og:title" content="렌모어" />
         <meta property="og:description" content="다양한 브랜드의 렌즈를 한 눈에! 렌즈모어는 " />
+        <meta property="og:image" content="/og-image.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="600" />
       </Head>
       <body>
         <Main />
